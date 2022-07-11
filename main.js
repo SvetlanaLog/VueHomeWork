c8d4f252aca2831544959b70f47ee412a6df3fb8
Светлана <sve-bashmakova@ya.ru>
2022-07-11 15:47:34 +0300

    image

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "A pair of warm, fuzzy socks"
+        image: "./assets/vmSocks-green.jpg"
     }
 })
 
