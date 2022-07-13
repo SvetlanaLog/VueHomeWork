ebc48bf620fc39a71e6613eae052e0323a4cd8ad
Светлана <sve-bashmakova@ya.ru>
2022-07-13 09:10:49 +0300

    v-if v-else

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: "Socks",
-        image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
-        altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
-    }
+      product: 'Socks',
+      image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+      inStock: true,
+    } 
 })
 
 
