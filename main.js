851672c0d06125dadfe429def348e36dac9a83ca
Светлана <sve-bashmakova@ya.ru>
2022-07-18 14:25:36 +0300

    fix

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
 var app = new Vue({
   el: '#app',
   data: {
-      product: "Socks",
-      image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
-      inStock: true,
-      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-      variants: [
-        {
-          variantId: 2234,
-          variantColor: "green",
-          variantImage: "./assets/vmSocks-green.jpg"    
-        },
-        {
-          variantId: 2235,
-          variantColor: "blue",
-          variantImage: "./assets/vmSocks-blue.jpg" 
-        }
-      ],
-      cart: 0
+    product: 'Socks',
+    image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+    inStock: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
+      }
+    ],
+    cart: 0
   },
   methods: {
     addToCart() {
@@ -28,6 +28,5 @@ var app = new Vue({
     }
   }
 })
-
 
 
